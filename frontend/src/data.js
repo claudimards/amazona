eb216b34e48338d3ea4073d6f405ffaee62e4a98
claudimar <claudimards@gmail.com>
2020-11-17 22:17:51 -0300

    Criado estrutura app em react

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirts',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+        {
+            _id: '2',
+            name: 'Nike Slim',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+        {
+            _id: '3',
+            name: 'Nike',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+        {
+            _id: '4',
+            name: 'Nike Slim Shirts',
+            category: 'Shirts',
+            image: '/images/p4.jpg',
+            price: 50,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+        {
+            _id: '5',
+            name: 'Nike Slim Shirts',
+            category: 'Shirts',
+            image: '/images/p5.jpg',
+            price: 80,
+            brand: 'Nike',
+            rating: 5.0,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+        {
+            _id: '6',
+            name: 'Nike Slim Shirts',
+            category: 'Pants',
+            image: '/images/p6.jpg',
+            price: 65,
+            brand: 'Nike',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+    ],
+};
+
+export default data;
