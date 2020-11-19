746224542053db791171042b15ff0e0f1a246e06
claudimar <claudimards@gmail.com>
2020-11-18 22:37:30 -0300

    Criada View do produto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
-import data from './data';
-import Product from './components/Product';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="grid-container">
         <header className="row">
             <div>
@@ -16,25 +18,15 @@ function App() {
         </header>
 
         <main>
-          <div>
-            <div className="row center">
-
-              {
-                data.products.map((product) => (
-
-                  <Product key={product._id} product={product}></Product>
-
-                ))
-              }
-          
-            </div>
-          </div>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
         </main>
 
         <footer className="row center">
             All right reserved
         </footer>
     </div>
+    </BrowserRouter>
   );
 }
 
