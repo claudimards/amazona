5367c135d3d5e8b1272f854c3eb73dc4070e74ca
claudimar <claudimards@gmail.com>
2020-12-01 22:23:44 -0300

    Implementado View histórico de pedidos

--- frontend/src/actions/OrderActions.js
+++ frontend/src/actions/OrderActions.js
@@ -8,7 +8,10 @@ import {ORDER_PAY_REQUEST,
     ORDER_DETAILS_FAIL, 
     ORDER_DETAILS_REQUEST, 
     ORDER_DETAILS_SUCCESS,
-    ORDER_PAY_SUCCESS} 
+    ORDER_PAY_SUCCESS,
+    ORDER_MINE_LIST_REQUEST,
+    ORDER_MINE_LIST_FAIL,
+    ORDER_MINE_LIST_SUCCESS} 
     from '../constants/OrderConstants';
 
 export const createOrder = (order) => async (dispatch, getState) => {
@@ -62,5 +65,23 @@ export const payOrder = (order, paymentResult) => async (dispatch, getState) => 
             ? error.response.data.message
             : error.message;
         dispatch({type: ORDER_PAY_FAIL, payload: message});
+    }
+};
+
+export const listOrderMine = () => async (dispatch, getState) => {
+    dispatch({type: ORDER_MINE_LIST_REQUEST});
+    const {userSignin: {userInfo}} = getState();
+    try {
+        const {data} = await Axios.get('/api/orders/mine', {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+        });
+        dispatch({type: ORDER_MINE_LIST_SUCCESS, payload: data});
+    } catch (error) {
+        const message = error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message;
+        dispatch({type: ORDER_MINE_LIST_FAIL, payload: message});
     }
 };
